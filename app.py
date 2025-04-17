c1e1bf9ce5cb817112050138270d7bd4a31d4bb1
caleb-yohannes2003 <caleb.yohannes2003@gmail.com>
2025-04-17 15:48:58 -0400

    app.py

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def sms_reply():
     return Response(str(twiml), mimetype="application/xml")
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
